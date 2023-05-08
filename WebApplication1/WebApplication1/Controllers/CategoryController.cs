e3809ec7927884d0e2d33c6d10a5f8e01a611ac6
Bikram Shrestha <stha.bikram999@gmail.com>
2023-05-09 00:43:20 +0545

    Displaying category

--- WebApplication1/WebApplication1/Controllers/CategoryController.cs
+++ WebApplication1/WebApplication1/Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.data;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class CategoryController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+           _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> categories = _context.Categories;
+            return View(categories);
         }
     }
 }
