0a92a749acdda326daf4115d1dda077957e9ff30
Bikram Shrestha <stha.bikram999@gmail.com>
2023-05-11 18:51:37 +0545

    Edit get method

--- WebApplication1/WebApplication1/Controllers/CategoryController.cs
+++ WebApplication1/WebApplication1/Controllers/CategoryController.cs
@@ -36,5 +36,31 @@ namespace WebApplication1.Controllers
             }
             return View(category);
         }
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id== null || id==0 )
+            {
+                return NotFound();
+            }
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
     }
 }
